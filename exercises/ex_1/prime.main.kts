20098fecb923a2ed7bac845cc2ef3f93d9f30995
nik-etl <niklas.etling@gmail.com>
2025-11-12 13:58:50 +0200

    add exercise and project folders

--- exercises/ex_1/prime.main.kts
+++ exercises/ex_1/prime.main.kts
@@ -1,2 +1,24 @@
 #!/usr/bin/env kotlin
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    for (i in 2 until num) {
+        if (num % i == 0) return false
+    }
+    return true
+}
 
+fun findPrimes(count: Int): List<Int> {
+    val primes = mutableListOf<Int>()
+    var num = 2
+    while (primes.size < 50) {
+        if (isPrime(num)) primes.add(num)
+        num++
+    }
+    return primes
+}
+
+fun main() {
+    val primeNumList = findPrimes(50)
+    println("First 50 prime numbers:\n$primeNumList")
+}
+main()
--- exercises/ex_2/func.main.kts
+++ exercises/ex_2/func.main.kts
@@ -1,2 +1,29 @@
-#!/usr/bin/env kotlin
+//#!/usr/bin/env kotlin
+//Practice functions and higher-order functions (filter, map, reduce) in Kotlin.
+//
+//Task
+//Create a list of integers (e.g., 1..100 or any 20+ integers).
+//
+//Use:
+//
+//filter { ... } → even numbers
+//
+//        map { ... } → squares of each number
+//
+//        reduce { acc, n -> ... } → sum of all numbers
+//
+//        Print results with clear labels:
+//
+//
+//Evens:
+//Squares:
+//Sum:
+val numbers = (1..100).toList()
 
+val evens = numbers.filter { it % 2 == 0 }
+val squares = numbers.map { it * it }
+val sum = numbers.reduce { acc, n -> acc + n }
+
+println("Evens: $evens")
+println("Squares: $squares")
+println("Sum: $sum")

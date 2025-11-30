5218f7e58265207629fd2b26a94844a2457edf3f
Nik-etl <156366742+Nik-etl@users.noreply.github.com>
2025-11-30 19:39:41 +0200

    Refactor Kotlin script to include main function

--- exercises/ex_2/lists.main.kts
+++ exercises/ex_2/lists.main.kts
@@ -1,15 +1,14 @@
 #!/usr/bin/env kotlin
 
-#!/usr/bin/env kotlin
 val numbers = (1..100).toList()
 
 val evens = numbers.filter { it % 2 == 0 }
 val squares = numbers.map { it * it }
 val sum = numbers.reduce { acc, n -> acc + n }
 
+
+fun main() {
 println("Evens: $evens")
 println("Squares: $squares")
 println("Sum: $sum")
-
-
-
+}

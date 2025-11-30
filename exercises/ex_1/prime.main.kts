00b99f211a4db4a0061b127821b236680a4c7d4c
Nik-etl <156366742+Nik-etl@users.noreply.github.com>
2025-11-30 19:34:38 +0200

    Implement prime number function

--- exercises/ex_1/prime.main.kts
+++ exercises/ex_1/prime.main.kts
@@ -1,41 +1,22 @@
-#!/usr/bin/env kotlin
-
-open class Employee(val name: String, protected val salary: Int) {
-    open fun calculateSalary(): Int {
-        return salary
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    for (i in 2 until num) {
+        if (num % i == 0) return false
     }
+    return true
 }
 
-class FullTimeEmployee(
-    name: String,
-    baseSalary: Int,
-    private val bonus: Int
-) : Employee(name, baseSalary) {
-    override fun calculateSalary(): Int {
-        return salary + bonus
+fun findPrimes(count: Int): List<Int> {
+    val primes = mutableListOf<Int>()
+    var num = 2
+    while (primes.size < 50) {
+        if (isPrime(num)) primes.add(num)
+        num++
     }
+    return primes
 }
 
-class PartTimeEmployee(
-    name: String,
-    private val hourlyRate: Int,
-    private val hoursWorked: Int
-) : Employee(name, 0) {
-    override fun calculateSalary(): Int {
-        return hourlyRate * hoursWorked
-    }
-}
-
-// script runs directly
-val emp1 = FullTimeEmployee("John", 100100, 25000)
-val emp2 = FullTimeEmployee("Jane", 87000, 25000)
-val emp3 = PartTimeEmployee("James", 15, 45)
-
-val employees = mapOf(
-    emp1.name to emp1,
-    emp2.name to emp2,
-    emp3.name to emp3
-)
-for ((name, employee) in employees) {
-    println("Salary of $name: ${employee.calculateSalary()}")
+fun main() {
+    val primeNumList = findPrimes(50)
+    println("First 50 prime numbers:\n$primeNumList")
 }
